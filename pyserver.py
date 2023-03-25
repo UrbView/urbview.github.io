8480e61136678a09f91de2404a7d955a5d8ba0ce
EauDeData <amolina@cvc.uab.cat>
2023-03-25 15:27:35 +0100

    now it works

--- pyserver.py
+++ pyserver.py
@@ -5,9 +5,43 @@ import os
 import warnings
 import json
 
+import torch, torchvision
+import numpy as np
+from pytorch_grad_cam import GradCAM
+import base64
+import cv2
+from PIL import Image
+
+
+class SimilarityToConceptTarget:
+
+    # Source: https://github.com/jacobgil/pytorch-grad-cam/blob/master/tutorials/Pixel%20Attribution%20for%20embeddings.ipynb
+
+    def __init__(self, features):
+        self.features = features
+    
+    def __call__(self, model_output):
+        cos = torch.nn.CosineSimilarity(dim=0)
+        return cos(model_output, self.features)
+    
+
+class GradCamScanner:
+    def __init__(self, model, target, layers, device = 'cuda') -> None:
+        self.model = model
+        self.target = target
+        self.target_layers = layers
+        self.use_cuda = device == 'cuda'
+
+    def scan(self, image):
+        with GradCAM(model=self.model, target_layers=self.target_layers, use_cuda=False) as cam:
+            return cam(input_tensor=image[None,], targets=[self.target])[0, :]  
+
+
+
 html_dir = os.path.dirname(os.path.realpath(__file__)) + '/'
 app = Flask(__name__, template_folder='.')
 warnings.warn("Be careful! This server isn't protected ad therefore should only be used in debugging; never in production.")
+model = torchvision.models.resnet50(pretrained = 'imagenet').cuda()
 
 @app.route('/<path:path>/<file>.<extension>')
 def file(path, file, extension):
@@ -17,20 +51,48 @@ def file(path, file, extension):
 def route(path):
     return render_template(f"{path}/index.html")
 
+def np_encoder(object):
+    if isinstance(object, np.generic):
+        return object.item()
+
 @app.route('/', methods = ['POST'])
 def index():
 
-    json_string = {
-            'response': [
-                {'confidence': 98.32, 'keywords': ['good','yes','green'], 'label': 0},
-                {'confidence': 12.34, 'keywords': ['bad','no','red'], 'label': 1},
-                {'confidence': 78.32, 'keywords': ['good','yes','green'], 'label': 1}
-            ],
-            'aggregation': {'confidence': 62.32, 'keywords': ['good','yes','green'], 'label': 0},
-            'heatmap': "iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAYAAAByDd+UAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAATLSURBVEhLjZZrets2EEVBkJQiO/vfQ5fQXeWLm9qyRKLnXICS0z8t7SuQwGAu5oEBprc//2in1+/l9O1bKe/vQfvrV2lvb+BXKR/X0j4+S9m3MpVWfPalln2dy3Raynw+BWVZS5nXckX+148f5f3nW1lP57Kup7K+vBQ51tfXUutUUaSW/QkV11oKSpvKeW0TdPbNyIMq+J7oz7MzZ9uCideJscgsc1poFOJ9ZqXtKawlEk4QaUFjvNUppIV24ptJZQ4p3w9C5t3vZYonXK/jyC14QUI7G4RTqaXd97JdP8t2u5WNSY2BKI/buuumdYAVTyHCCokGX7yCBbYuZEZOY0StzEGfsjYQ3sr946PcP2mxcmPurvuISyU+9fKtzGA6EScs191xsTyPR00sH+ULVp1Op7Szbg9mwiG5E7Fqg3CDeCOGO3MTLy1i4kxCSRwXQ9iwoCkDxe+kUKLvIE24QqZpndhfhEACjBAEFYLp5VLK60tpWOUCdlQ3Y60lyuumodDv9Dts8ijnP+8Nj7V7jy/ug9AJYcdc/L6cVyw6lyohaFiJzSTvFtI8rthEQF43+R6PGvugkzbmbIbIvLhBCEylEJoFWplVQ1KM1/lMzHDjCHigMXzHVc6RR+Ubi7qxKKAlbi/7hN+GrZCUnfBwiUBJt4B+yNIeiwrGuPBx37Ly3YS7Xmk/48IJosqYmMRwa+0Ku4U984SK6Y+7DsIhd8hCqIMTI7eT24qKZKv7JrbaRLo/SQ/CwzIXi8CNldxR4EoLk60aJpNbYkr5gliPkBB74tNjtG8dLkCr1YgU6G7PFmWOgegr5nUjFp9MvuGe/XorhbYTsh/XM7GVkJg6d8RMsjsW3Wk3SbWExOm+kpC/Bk0mxXc8rvhYuWIQ7yhoECalI2OiHMmC3FjogZS40R7JFCRjMY+FaH0ikbqH2xYsOFHdZ+PFajYItcDUbrjJuarJFsLSqiwnwsK+Xdi/K/N7dWGcRXWrRpbiBTOVsoH5bIcZYSHpXMlOCREyRrvgW8KsHm/o5shbwtg+K5VoJc5HhXmcQsO6HAwSRoFuQijVYxJYfECLkPnvp7uUSUe2pKs/Y6wPdUJdag2MewdZSl7I/gceZAfoDmkf63GlzmZT4q74K0BoIDFAKHuTCc992uHYobCTwDAQr+Vdgzrs5zjHx8O/wsBmM1uWRtw6vySEPPhC9IDKvxSHA9ZZTpfju3blg+grzMoA0mGR2FHcAr5F+lnQUJhcSE1+4kEqb/t4LxsXp7vwEM5B/Dz9dz0QKyELcMi/YJ/nYw5nIUFcOKAHZOOpDSIJb+9/l9u1kwUhHNshwt0SlXfSKQiZeLgPtwZfiRHofNRVK3zwmYqfChM3s/bjbIskM1RANudKyKYP/NaN3nUC3LgoCyQZRGNZEHqcYM1EHAM2aVIDN/St4UTBOwWhLt4ILmV++Q64Z14uXD+osxZ3CkE/lLUM/SEzHk9g4Qfn2ZXjBFLJ7GQM9RBIIniPFSr1jnMpCxfbWVxeciWplLhcsjy8Icy9B1IDkj8JCU+t/ogHUffeA/zkwivps/ML3AqHVWIQIfYAevNDAlZPZje+pDOdAeN960jWCUOq0t/AJTnwwjxuByE+SLWNltRyi5V9L/8A90bB3A5jOCQAAAAASUVORK5CYII="
-            }
+    # Get the file from the request
+    file = request.files['file']
 
-    response = Response(response=json.dumps(json_string), status=200, mimetype="application/json")
+    # Check if the file is empty
+    if file.filename == '':
+        return 'errors.EmptyFileError'
+    print(file)
+    data = np.array(Image.open(file).convert('RGB').resize((224, 224)))
+    print(data.shape)
+
+    data = np.stack([data, np.roll(data, 10, 1), np.roll(data, -10, 1), np.roll(data, 10, 0), np.roll(data, -10, 0)]).transpose(0, 3, 1, 2).astype(np.uint8) / 255
+    print(data.shape)
+    data = torch.from_numpy(data).float().cuda()
+    with torch.no_grad():
+        out = torch.nn.functional.sigmoid(model(data)).cpu().numpy()
+        response = [{'label': np.argmax(o), 'confidence': np.max(o), 'keywords': ['Not', 'Implemented', 'Yet']} for o in out]
+    
+    layers = list(model.modules())[:-5]
+    concept = model(data)[0]
+    scanner = GradCamScanner(model, SimilarityToConceptTarget(concept.detach()), layers)
+
+    p = 0.61
+
+    heatmap = scanner.scan(data[0])
+    heatmap = 255 * (heatmap - heatmap.min()) / (heatmap.max() - heatmap.min())
+    image = (255 * data[0].cpu().numpy().mean(0)) * (1 - p) + heatmap * p
+    cv2.imwrite('tmp.png', image.astype(np.uint8))
+    with open("tmp.png", "rb") as image_file:
+        b64 = base64.b64encode(image_file.read()).decode()
+    
+
+    
+    json_string = {'response': response, 'heatmap': b64 , 'aggregation': response[0]}
+
+    response = Response(response=json.dumps(json_string, default=np_encoder), status=200, mimetype="application/json")
     # Add an 'Access-Control-Allow-Origin' header to the response to allow requests from any origin
     response.headers.add('Access-Control-Allow-Origin', '*')
 
