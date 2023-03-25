23977ba0e8808817d6883532a2f63bdcd393f56a
IararIV <gerard.jover2000@gmail.com>
2023-03-25 12:57:58 +0100

    Test upload plan

--- pyserver.py
+++ pyserver.py
@@ -17,9 +17,17 @@ def route(path):
 
 @app.route('/')
 def index():
-    return render_template(f"index.html")
+    return """{
+            response: [
+                {confidence: 98.32, keywords: ['good','yes','green'], label: true},
+                {confidence: 12.34, keywords: ['bad','no','red'], label: false},
+                {confidence: 78.32, keywords: ['good','yes','green'], label: true}
+            ],
+            aggregation: {confidence: 62.32, keywords: ['good','yes','green'], label: true},
+            heatmap: "iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAYAAAByDd+UAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAATLSURBVEhLjZZrets2EEVBkJQiO/vfQ5fQXeWLm9qyRKLnXICS0z8t7SuQwGAu5oEBprc//2in1+/l9O1bKe/vQfvrV2lvb+BXKR/X0j4+S9m3MpVWfPalln2dy3Raynw+BWVZS5nXckX+148f5f3nW1lP57Kup7K+vBQ51tfXUutUUaSW/QkV11oKSpvKeW0TdPbNyIMq+J7oz7MzZ9uCideJscgsc1poFOJ9ZqXtKawlEk4QaUFjvNUppIV24ptJZQ4p3w9C5t3vZYonXK/jyC14QUI7G4RTqaXd97JdP8t2u5WNSY2BKI/buuumdYAVTyHCCokGX7yCBbYuZEZOY0StzEGfsjYQ3sr946PcP2mxcmPurvuISyU+9fKtzGA6EScs191xsTyPR00sH+ULVp1Op7Szbg9mwiG5E7Fqg3CDeCOGO3MTLy1i4kxCSRwXQ9iwoCkDxe+kUKLvIE24QqZpndhfhEACjBAEFYLp5VLK60tpWOUCdlQ3Y60lyuumodDv9Dts8ijnP+8Nj7V7jy/ug9AJYcdc/L6cVyw6lyohaFiJzSTvFtI8rthEQF43+R6PGvugkzbmbIbIvLhBCEylEJoFWplVQ1KM1/lMzHDjCHigMXzHVc6RR+Ubi7qxKKAlbi/7hN+GrZCUnfBwiUBJt4B+yNIeiwrGuPBx37Ly3YS7Xmk/48IJosqYmMRwa+0Ku4U984SK6Y+7DsIhd8hCqIMTI7eT24qKZKv7JrbaRLo/SQ/CwzIXi8CNldxR4EoLk60aJpNbYkr5gliPkBB74tNjtG8dLkCr1YgU6G7PFmWOgegr5nUjFp9MvuGe/XorhbYTsh/XM7GVkJg6d8RMsjsW3Wk3SbWExOm+kpC/Bk0mxXc8rvhYuWIQ7yhoECalI2OiHMmC3FjogZS40R7JFCRjMY+FaH0ikbqH2xYsOFHdZ+PFajYItcDUbrjJuarJFsLSqiwnwsK+Xdi/K/N7dWGcRXWrRpbiBTOVsoH5bIcZYSHpXMlOCREyRrvgW8KsHm/o5shbwtg+K5VoJc5HhXmcQsO6HAwSRoFuQijVYxJYfECLkPnvp7uUSUe2pKs/Y6wPdUJdag2MewdZSl7I/gceZAfoDmkf63GlzmZT4q74K0BoIDFAKHuTCc992uHYobCTwDAQr+Vdgzrs5zjHx8O/wsBmM1uWRtw6vySEPPhC9IDKvxSHA9ZZTpfju3blg+grzMoA0mGR2FHcAr5F+lnQUJhcSE1+4kEqb/t4LxsXp7vwEM5B/Dz9dz0QKyELcMi/YJ/nYw5nIUFcOKAHZOOpDSIJb+9/l9u1kwUhHNshwt0SlXfSKQiZeLgPtwZfiRHofNRVK3zwmYqfChM3s/bjbIskM1RANudKyKYP/NaN3nUC3LgoCyQZRGNZEHqcYM1EHAM2aVIDN/St4UTBOwWhLt4ILmV++Q64Z14uXD+osxZ3CkE/lLUM/SEzHk9g4Qfn2ZXjBFLJ7GQM9RBIIniPFSr1jnMpCxfbWVxeciWplLhcsjy8Icy9B1IDkj8JCU+t/ogHUffeA/zkwivps/ML3AqHVWIQIfYAevNDAlZPZje+pDOdAeN960jWCUOq0t/AJTnwwjxuByE+SLWNltRyi5V9L/8A90bB3A5jOCQAAAAASUVORK5CYII="
+            }"""
 
-ip = "0.0.0.0:5000"
+ip = "localhost:8080" #"0.0.0.0:5000"
 ip, port = ip.split(':')
 
 app.run(host=ip, port = int(port[:]), debug = True)
